e7e9eabb1de9effc203b1131fe83947b851c8761
iabbene <ianabbene@gmail.com>
2023-08-29 21:53:15 -0500

    Component skeleton, setup CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
+import { MovieItemComponent } from './components/movies/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    MovieListComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
